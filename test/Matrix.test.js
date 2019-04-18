24d482a3960a382b9f89ab8036e94b1d62eb5f59
Keren-Happuc Fleurimond <keren@noukod.com>
2019-04-18 15:54:55 +0000

    Automatically backed up by Learn

--- test/Matrix.test.js
+++ test/Matrix.test.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { configure, shallow } from 'enzyme';
-import { expect } from 'chai';
-// import sinon from 'sinon';
+import './index.css';
+import chromeBoi from './data.js'
+import Matrix from './Matrix.js'
 
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
-
-import Matrix from '../src/Matrix'
-
-
-describe('<Matrix />', () => {
-  
-  it("change me to true in test/matrix.test.js once your app is working!", () => {
-    // did you make sure to pass a function as a prop to Cell instead of the selected value itself?
-    expect(false).to.equal(true)
-  })
-
-})
+ReactDOM.render(
+    <Matrix values={chromeBoi.slice(0, 10)} />,
+    document.getElementById('root')
+);
